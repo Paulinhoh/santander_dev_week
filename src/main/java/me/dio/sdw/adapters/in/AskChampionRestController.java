377b74b1d137f492ba10aafb3d7f1af791e448f8
Paulinhoh <phdsreis@gmail.com>
2024-03-27 23:14:49 -0300

    docs: updating adapters

--- src/main/java/me/dio/sdw/adapters/in/AskChampionRestController.java
+++ src/main/java/me/dio/sdw/adapters/in/AskChampionRestController.java
@@ -3,6 +3,7 @@ package me.dio.sdw.adapters.in;
 import me.dio.sdw.application.AskChampionUseCase;
 
 import io.swagger.v3.oas.annotations.tags.Tag;
+
 import org.springframework.web.bind.annotation.*;
 
 @Tag(name = "Campeões", description = "Endpoints do dominio de Campeões do Lol")
@@ -10,6 +11,7 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/champions")
 public record AskChampionRestController(AskChampionUseCase useCase) {
 
+    @CrossOrigin
     @PostMapping("/{championId}/ask")
     public AskChampionResponse askChampion(@PathVariable Long championId, @RequestBody AskChampionRequest resquest) {
         String answer = useCase.askChampion(championId, resquest.question());
